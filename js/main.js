ff945a2c033488d9c87427c98342807175fe4385
biorubenfs <biorubenfs@hotmail.com>
2021-03-12 19:11:00 +0100

    Modificado estilo del juego. Agregada funcionalidad pantalla de inicio y pantalla de lucha

--- js/main.js
+++ js/main.js
@@ -1,110 +1,126 @@
-// Las clases principales que dan lugar a los luchadores
+const jugadores = 2;
 
-class Fighter {
+// Personajes de frutas: las clases
+class Fruta {
     constructor() {
-        this.health = 100;
-        this.id = Fighter.id++;
+        this.puntosSalud = 100;
+        this.id = Fruta.id++;
     }
 }
-
-Fighter.id = 0;
 
 // Los objetos creados son tal cual están definidas, no dejan al usuario seleccionar nada
-class FighterRed extends Fighter {
+class Manzana extends Fruta {
     constructor() {
         super();
-        this.attack = 20;
-        this.color = "red";
+        this.nombre = "manzana";
+        this.puntosAtaque = 20;
+        // this.color = "red";
     }
 }
 
-class FighterBlue extends Fighter {
+class Platano extends Fruta {
     constructor() {
         super();
-        this.attack = 20;
-        this.color = "blue";
+        this.nombre = "platano";
+        this.puntosAtaque = 20;
+        // this.color = "blue";
     }
 }
 
-class FighterYellow extends Fighter {
+class Fresa extends Fruta {
     constructor() {
         super();
-        this.attack = 20;
-        this.color = "yellow";
+        this.nombre = "fresa";
+        this.puntosAtaque = 20;
+        // this.color = "yellow";
     }
 }
 
-class FighterGreen extends Fighter {
+class Pera extends Fruta {
     constructor() {
         super();
-        this.attack = 20;
-        this.color = "green";
+        this.nombre = "pera";
+        this.puntosAtaque = 20;
+        // this.color = "green";
     }
+}
+
+Fruta.id = 0;
+
+// Array en el que se almacenarán las frutas seleccionadas (hasta un máximo de 2).
+let frutas = [];
+
+// Agrega una fruta al array de frutas
+const addFruta = (arrayFrutas, fruta) => {
+    arrayFrutas.push(fruta);
+}
+
+// Cambiar la fruta escogida a seleccionada
+const displayFrutaInicio = (fruta) => {
+    let htmlElement = document.getElementById("displayCharacters");
+    htmlElement.appendChild(document.getElementById(fruta.nombre));
+}
+
+// Comprobación del array
+const checkArray = () => {
+    if (frutas.length == jugadores) {
+        document.getElementById("opciones-frutas").style.display = "none";
+        return true;
+    }
+    else false;
 }
 
 // Array en el que se almacenarán los luchadores según se vayan creando
 let players = [];
 
-// Agrega un jugador al array de jugadores
-const addCharacter = (arrayPlayers, player) => {
-    arrayPlayers.push(player);
-}
-
-// Agrega el jugador al html
-const displayCharacter = (color) => {
-    let htmlElement = document.getElementById("displayCharacters");
-    htmlElement.appendChild(document.getElementById(color));
-};
-
-// Comprobación del array
-const checkArray = () => {
-    console.log("Hemos llamado al checkArray()");
-    if (players.length == 2) {
-        document.getElementById("selection").style.display = "none";
-    }
-}
-
-// Jugadores
-let addPlayerBlue = () => {
-    let blue = new FighterBlue();
-    addCharacter(players, blue);
-    displayCharacter(blue.color);
-    checkArray();
-
-}
-
-let addPlayerGreen = () => {
-    let green = new FighterGreen();
-    addCharacter(players, green);
-    displayCharacter(green.color);
+// Creación de las instancias de Fruta, lo que vienen siendo los jugadores.
+let addManzana = () => {
+    let manzana = new Manzana();
+    addFruta(frutas, manzana);
+    displayFrutaInicio(manzana);
+    eliminarSeleccion(manzana);
     checkArray();
 }
 
-let addPlayerRed = () => {
-    let red = new FighterRed();
-    addCharacter(players, red);
-    displayCharacter(red.color);
+let addPera = () => {
+    let pera = new Pera();
+    addFruta(frutas, pera);
+    displayFrutaInicio(pera);
+    eliminarSeleccion(pera)
     checkArray();
 }
 
-let addPlayerYellow = () => {
-    let yellow = new FighterYellow();
-    addCharacter(players, yellow);
-    displayCharacter(yellow.color);
+let addPlatano = () => {
+    let platano = new Platano();
+    addFruta(frutas, platano);
+    displayFrutaInicio(platano);
+    eliminarSeleccion(platano)
     checkArray();
 }
 
-// Los personaje seleccionados están ahora en un array
+let addFresa = () => {
+    let fresa = new Fresa();
+    addFruta(frutas, fresa);
+    displayFrutaInicio(fresa);
+    eliminarSeleccion(fresa)
+    checkArray();
+}
 
+let eliminarSeleccion = (fruta) => {
+    document.getElementById(fruta.nombre).classList.remove("hover-effect");
+}
+
+
+// Las frutas seleccionadas están ahora en un array
 //fight(players[0], players[1]);
 
 // Comprobando el ganador
-let checkWinner = (player1, player2) => {
-    if (player1.health <= 0) {
-        alert(`${player2.color} WINS!`)
+let checkWinner = (fruta1, fruta2) => {
+    if (fruta1.puntosSalud <= 0) {
+        alert(`${fruta2.nombre} WINS!`)
     }
-    else if (player2.health <= 0) {
-        alert(`${player1.color} WINS!`)
+    else if (fruta2.puntosSalud <= 0) {
+        alert(`${fruta1.nombre} WINS!`)
     }
     else {
         console.log("El combate sigue");
@@ -112,23 +128,41 @@ let checkWinner = (player1, player2) => {
 };
 
 let atacarPlayer1 = () => {
-    players[1].health = players[1].health - players[0].attack;
-    console.log(`${players[1].color} ps: ${players[1].health}`);
-    let ps = `${players[1].health}%`;
+    frutas[1].puntosSalud = frutas[1].puntosSalud - frutas[0].puntosAtaque;
+    console.log(`${frutas[1].nombre} ps: ${frutas[1].puntosSalud}`);
+    let ps = `${frutas[1].puntosSalud}%`;
     document.getElementById("ps-2").style.width = ps;
 
-    checkWinner(players[0], players[1]);
+    // Verificando si existe un ganador después de cada ataque
+    checkWinner(frutas[0], frutas[1]);
 
 }
 
 let atacarPlayer2 = () => {
-    players[0].health = players[0].health - players[1].attack;
-    console.log(`${players[0].color} ps: ${players[0].health}`);
-    let ps = `${players[0].health}%`;
-
+    frutas[0].puntosSalud = frutas[0].puntosSalud - frutas[1].puntosAtaque;
+    console.log(`${frutas[0].nombre} ps: ${frutas[0].puntosSalud}`);
+    let ps = `${frutas[0].puntosSalud}%`;
     document.getElementById("ps-1").style.width = ps;
 
+    // Verificando si existe un ganador después de cada ataque
     checkWinner(players[0], players[1]);
+}
+
+const displayFrutasLucha = () => {
+    let htmlElement = document.getElementById("luchadores");
+    htmlElement.appendChild(document.getElementById(frutas[0].nombre));
+    htmlElement.appendChild(document.getElementById(frutas[1].nombre));
+}
+
+const start = () => {
+    if (checkArray()) {
+        document.getElementById("inicio").style.display = "none";
+        document.getElementById("lucha").style.display = "flex";
+        displayFrutasLucha();
+    }
+    else {
+        alert("Tienes que elegir dos jugadores");
+    }
 }
 
 
---
+++ js/main_copy.js
@@ -0,0 +1,135 @@
+// Las clases principales que dan lugar a los luchadores
+
+class Fighter {
+    constructor() {
+        this.health = 100;
+        this.id = Fighter.id++;
+    }
+}
+
+Fighter.id = 0;
+
+// Los objetos creados son tal cual están definidas, no dejan al usuario seleccionar nada
+class FighterRed extends Fighter {
+    constructor() {
+        super();
+        this.attack = 20;
+        this.color = "red";
+    }
+}
+
+class FighterBlue extends Fighter {
+    constructor() {
+        super();
+        this.attack = 20;
+        this.color = "blue";
+    }
+}
+
+class FighterYellow extends Fighter {
+    constructor() {
+        super();
+        this.attack = 20;
+        this.color = "yellow";
+    }
+}
+
+class FighterGreen extends Fighter {
+    constructor() {
+        super();
+        this.attack = 20;
+        this.color = "green";
+    }
+}
+
+// Array en el que se almacenarán los luchadores según se vayan creando
+let players = [];
+
+// Agrega un jugador al array de jugadores
+const addCharacter = (arrayPlayers, player) => {
+    arrayPlayers.push(player);
+}
+
+// Agrega el jugador al html
+const displayCharacter = (color) => {
+    let htmlElement = document.getElementById("displayCharacters");
+    htmlElement.appendChild(document.getElementById(color));
+};
+
+// Comprobación del array
+const checkArray = () => {
+    console.log("Hemos llamado al checkArray()");
+    if (players.length == 2) {
+        document.getElementById("selection").style.display = "none";
+    }
+}
+
+// Jugadores
+let addPlayerBlue = () => {
+    let blue = new FighterBlue();
+    addCharacter(players, blue);
+    displayCharacter(blue.color);
+    checkArray();
+
+}
+
+let addPlayerGreen = () => {
+    let green = new FighterGreen();
+    addCharacter(players, green);
+    displayCharacter(green.color);
+    checkArray();
+}
+
+let addPlayerRed = () => {
+    let red = new FighterRed();
+    addCharacter(players, red);
+    displayCharacter(red.color);
+    checkArray();
+}
+
+let addPlayerYellow = () => {
+    let yellow = new FighterYellow();
+    addCharacter(players, yellow);
+    displayCharacter(yellow.color);
+    checkArray();
+}
+
+// Los personaje seleccionados están ahora en un array
+
+//fight(players[0], players[1]);
+
+// Comprobando el ganador
+let checkWinner = (player1, player2) => {
+    if (player1.health <= 0) {
+        alert(`${player2.color} WINS!`)
+    }
+    else if (player2.health <= 0) {
+        alert(`${player1.color} WINS!`)
+    }
+    else {
+        console.log("El combate sigue");
+    }
+};
+
+let atacarPlayer1 = () => {
+    players[1].health = players[1].health - players[0].attack;
+    console.log(`${players[1].color} ps: ${players[1].health}`);
+    let ps = `${players[1].health}%`;
+    document.getElementById("ps-2").style.width = ps;
+
+    checkWinner(players[0], players[1]);
+
+}
+
+let atacarPlayer2 = () => {
+    players[0].health = players[0].health - players[1].attack;
+    console.log(`${players[0].color} ps: ${players[0].health}`);
+    let ps = `${players[0].health}%`;
+
+    document.getElementById("ps-1").style.width = ps;
+
+    checkWinner(players[0], players[1]);
+}
+
+/// IMPORTANTE; PONER EL NUMERO DE JUGADORES ARRIBA EN UNA CONSTANTE, QUE QUEDE BIEN CLARO
+
